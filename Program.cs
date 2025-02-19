216a0a38415a85a3b15d90cb3f83d5cfdc723dde
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-02-19 15:53:23 +0700

    message

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+for (int i = 0; i <= 5; i++)
+{
+    for (int j = 0; j <= 5; j++)
+    {
+        Console.WriteLine('*');
+    }
+    Console.WriteLine();
+}
